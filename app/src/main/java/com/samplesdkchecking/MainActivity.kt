bdf37e0e4860e237848c88d9b7d0fc342717b0a8
sandeep akuthota <sandeep.akuthota@gmail.com>
2021-10-13 10:46:54 +0530

    create project in studio create toast for easy popup

---
+++ app/src/main/java/com/samplesdkchecking/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.samplesdkchecking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       // "javatpoint"
+        //ToasterActivity.toastmessage(this,"this is sdk testing example")
+        ToasterActivity.toastmessage(this,"this is sdk testing example")
+    }
+}
+
+
---
+++ myapplication/src/main/java/com/toastmessage/ToastMessage.kt
@@ -0,0 +1,13 @@
+package com.toastmessage
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastMessage {
+
+    companion object {
+        fun toastmessage(c: Context?, message: String?) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
