b9b5fdc92b1c5d475debd598ace4f4e0a9df6c6d
sandeep akuthota <sandeep.akuthota@gmail.com>
2021-10-13 17:43:30 +0530

    Merge remote-tracking branch 'origin/master'

--- logmessage/src/main/java/com/logmessage/LogMessage.kt
+++ logmessage/src/main/java/com/logmessage/LogMessage.kt
@@ -1,4 +1,19 @@
 package com.logmessage
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
 class LogMessage {
+
+
+    companion object {
+        public var tag:String = "Application Library";
+        fun logmessage(c: Context?, message: String?) {
+
+            Log.i(tag, message.toString())
+
+
+        }
+    }
 }
